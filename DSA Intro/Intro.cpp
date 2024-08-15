9b6e35ce25367a7c0cbb40a683ca859ab6fe23bd
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-08-16 00:55:07 +0500

    Introduction

---
+++ DSA Intro/Intro.cpp
@@ -0,0 +1,21 @@
+/* Data Structures is about how data can be stored in different structures.
+
+Algorithms is about how to solve different problems, often by searching through and manipulating data structures.
+
+In Computer Science there are two different kinds of data structures, Primitive Data Structures and Abstract Data Structures
+
+Primitive Data Structures are basic data structures provided by programming languages to represent single values,
+ such as integers, floating-point numbers, characters, and booleans.
+
+Abstract Data Structures are higher-level data structures that are built using primitive data types and
+ provide more complex and specialized operations. Some common examples of abstract data structures
+  include arrays, linked lists, stacks, queues, trees, and graphs.
+  
+  
+  Importance:
+    Data structures are essential ingredients in creating fast and powerful algorithms. 
+    They help in managing and organizing data, reduce complexity, and increase efficiency.*/
+
+
+    // Roadmap:  https://roadmap.sh/datastructures-and-algorithms
+      
