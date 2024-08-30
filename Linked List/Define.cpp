43b7e54ef38b465c4ce067d74dbe09cc3aa0462d
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-08-30 22:48:23 +0500

    Add files via upload

---
+++ Linked List/Define.cpp
@@ -0,0 +1,15 @@
+/*Linked Lists are a type of data structure used for storing collections of data. The data is stored 
+  in nodes, each of which contains a data field and a reference (link) to the next node in the sequence.
+  Linked Lists are used in other types of data structures like stacks and queues.
+
+
+  Types: 
+     Two types of linked lists are commonly used: 
+       Singly linked lists, where each node points to the next node and the last node points to null. 
+       Doubly linked lists, where each node has two links, one to the previous node and another one to the next. 
+
+
+ Linked Lists vs Arrays (Comparison)
+        Linked lists consist of nodes, and is a linear data structure we make ourselves,
+        unlike arrays which is an existing data structure in the programming language that we can use.
+   */
