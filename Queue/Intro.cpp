1b1c450e4743c06b95ce13515bb3cf69abebaab0
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-09-14 12:26:04 +0500

    Add files via upload

---
+++ Queue/Intro.cpp
@@ -0,0 +1,21 @@
+/*  Queue is a linear data structure that follows a particular order in which the operations are
+    performed for storing data. The order is First In First Out (FIFO).
+    
+    This type of organization is particularly useful for specific situations such as printing jobs, 
+    handling requests in a web server, scheduling tasks in a system, etc.
+    
+    It is an ordered list in which insertions are done at one end which is known as the rear and
+    deletions are done from the other end known as the front.
+     
+     The difference between stacks and queues is in removing. In a stack we remove the item 
+     the most recently added; in a queue, we remove the item the least recently added.
+
+     Basic operations we can do on a queue are:
+
+         Enqueue: Adds a new element to the queue.
+         Dequeue: Removes and returns the first (front) element from the queue.
+         Peek: Returns the first element in the queue.
+         isEmpty: Checks if the queue is empty.
+         Size: Finds the number of elements in the queue.
+
+*/
